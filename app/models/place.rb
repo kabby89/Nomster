c6c931861ae785c55b057b7f32612613411a1ab6
Kellen McCarvel <kellen.mccarvel@gmail.com>
2014-12-16 23:06:17 +0000

    nomster lesson 28

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,11 @@
 class Place < ActiveRecord::Base
 	belongs_to :user
-	validates :name, :presence => :true
+	validates :name, :address, :presence => :true, uniqueness: true, :length => {
+		minimum: 3,
+		maximum: 45
+	} 
+	validates :description, :presence => :true, :length => {
+		minimum: 15,
+		maximum: 600
+	}
 end
