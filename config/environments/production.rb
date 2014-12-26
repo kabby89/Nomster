8fb51eeb941f8c28524722d7ce904da1612ff851
Kellen McCarvel <kellen.mccarvel@gmail.com>
2014-12-26 21:38:26 +0000

    nomster final

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,7 +6,7 @@ Nomster::Application.configure do
     :port => '587',
     :authentication => :plain,
     :user_name => ENV['SENDGRID_USERNAME'],
-    :password => ['SENDGRID_PASSWORD'],
+    :password => ENV['SENDGRID_PASSWORD'],
     :domain => 'heroku.com',
     :enable_starttls_auto => true
   }
